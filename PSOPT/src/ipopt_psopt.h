d60ce77059e00edb535c32a0fc35721dca474c4e
Conrad Poelman <cp@stellarscience.com>
2020-01-21 21:31:38 -0500

    Fix gcc warning-as-error with -Werror=suggest-override by adding "override" where necessary, even though this is 3rdparty code. (An alternative was to suppress warnings around the #includes but that approach is fragile.)

--- PSOPT/src/ipopt_psopt.h
+++ PSOPT/src/ipopt_psopt.h
@@ -59,30 +59,30 @@ public:
                                        Number alpha_du, Number alpha_pr,
                                        Index ls_trials,
                                        const IpoptData* ip_data,
-                                       IpoptCalculatedQuantities* ip_cq);
+                                       IpoptCalculatedQuantities* ip_cq) override;
 
 
   virtual bool get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
-                            Index& nnz_h_lag, IndexStyleEnum& index_style);
+                            Index& nnz_h_lag, IndexStyleEnum& index_style) override;
 
   /** Method to return the bounds for my problem */
   virtual bool get_bounds_info(Index n, Number* x_l, Number* x_u,
-                               Index m, Number* g_l, Number* g_u);
+                               Index m, Number* g_l, Number* g_u) override;
 
   /** Method to return the starting point for the algorithm */
   virtual bool get_starting_point(Index n, bool init_x, Number* x,
                                   bool init_z, Number* z_L, Number* z_U,
                                   Index m, bool init_lambda,
-                                  Number* lambda);
+                                  Number* lambda) override;
 
   /** Method to return the objective value */
-  virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value);
+  virtual bool eval_f(Index n, const Number* x, bool new_x, Number& obj_value) override;
 
   /** Method to return the gradient of the objective */
-  virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f);
+  virtual bool eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f) override;
 
   /** Method to return the constraint residuals */
-  virtual bool eval_g(Index n, const Number* x, bool new_x, Index m, Number* g);
+  virtual bool eval_g(Index n, const Number* x, bool new_x, Index m, Number* g) override;
 
   /** Method to return:
    *   1) The structure of the jacobian (if "values" is NULL)
@@ -90,7 +90,7 @@ public:
    */
   virtual bool eval_jac_g(Index n, const Number* x, bool new_x,
                           Index m, Index nele_jac, Index* iRow, Index *jCol,
-                          Number* values);
+                          Number* values) override;
 
   /** Method to return:
    *   1) The structure of the hessian of the lagrangian (if "values" is NULL)
@@ -99,7 +99,7 @@ public:
   virtual bool eval_h(Index n, const Number* x, bool new_x,
                       Number obj_factor, Index m, const Number* lambda,
                       bool new_lambda, Index nele_hess, Index* iRow,
-                      Index* jCol, Number* values);
+                      Index* jCol, Number* values) override;
 
   //@}
 
@@ -111,7 +111,7 @@ public:
                                  Index m, const Number* g, const Number* lambda,
                                  Number obj_value,
 				 const IpoptData* ip_data,
-				 IpoptCalculatedQuantities* ip_cq);
+				 IpoptCalculatedQuantities* ip_cq) override;
   //@}
 
 private:
